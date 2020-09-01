9ec9ba353b56538e7142d873cf3bd75df1e11474
teunvandorp2002 <teunvandorp2002@users.noreply.github.com>
2020-09-01 16:15:05 +0200

    Changed the GUI

--- src/Start.java
+++ src/Start.java
@@ -2,14 +2,17 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Start {
+
     public static void main(String[] args) {
         opdracht1();
         opdracht2();
         opdracht3();
     }
+
     public static void opdracht1(){
         JOptionPane.showMessageDialog(null, "Hello world");
     }
+
     public static void opdracht2(){
         String name = JOptionPane.showInputDialog(null, "Voer een naam in");
         JOptionPane.showMessageDialog(null, "Hello " + name);
@@ -23,12 +26,13 @@ public class Start {
         label.setBounds(50,100,200,30);
         JTextField textfield = new JTextField(SwingConstants.CENTER);
         textfield.setBounds(50,150,200,30);
-        textfield.setSize(200, 30);
+        textfield.setBounds(50, 150, 200, 30);
         panel.add(label);
         frame.add(panel);
         frame.add(textfield);
         frame.add(label);
         frame.setSize(300, 300);
+        frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
